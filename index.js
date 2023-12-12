beaceaa8597e6b734ac0cb359e82380a6ca27cac
Caio <caio002006@gmail.com>
2023-12-12 18:19:45 -0300

    Classificador de nível de herói

---
+++ index.js
@@ -0,0 +1,25 @@
+let name = "Ragnar"
+let xp = 10002
+
+if (xp < 1000){
+    console.log(`O herói de nome ${name} está no nível de Ferro`)
+} else if (xp > 1000 && xp <= 2000){
+    console.log(`O herói de nome ${name} está no nível de Bronze`)
+} else if (xp > 2000 && xp <= 5000){
+    console.log(`O herói de nome ${name} está no nível de Prata`)
+
+} else if (xp > 6000 && xp <= 7000){
+    console.log(`O herói de nome ${name} está no nível de Ouro`)
+
+} else if (xp > 7000 && xp <= 8000){
+    console.log(`O herói de nome ${name} está no nível de Platina`)
+
+} else if (xp > 8000 && xp <= 9000){
+    console.log(`O herói de nome ${name} está no nível de Ascendente`)
+
+} else if (xp > 9000 && xp <= 10000){
+    console.log(`O herói de nome ${name} está no nível de Imortal`)
+
+} else if (xp > 10000){
+    console.log(`O herói de nome ${name} está no nível de Radiante`)
+}
